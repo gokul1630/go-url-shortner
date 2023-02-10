e53159128ccb5f075130e1fb44ee2111ab44328a
gokul1630 <gokulgp3167@gmail.com>
2023-02-10 13:29:53 +0530

    Added MongoDB drivers

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"crypto/rand"
 	"log"
 	"math/big"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -21,6 +26,16 @@ func main() {
 	ok := godotenv.Load()
 	err(ok)
 
+	MONGOURI := os.Getenv("MONGO_URI")
+
+	client, error := mongo.Connect(context.TODO(), options.Client().ApplyURI(MONGOURI))
+
+	err(error)
+
+	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+		panic(err)
+	}
+
 	router := gin.Default()
 
 	router.GET("/", func(context *gin.Context) {
@@ -31,6 +46,11 @@ func main() {
 
 	log.Fatal(router.Run(":3000"))
 
+	defer func() {
+		error := client.Disconnect(context.TODO())
+		err(error)
+
+	}()
 }
 
 func handleNewUrl(context *gin.Context) {
