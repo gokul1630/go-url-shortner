ee8db1539bb19ec68081a1c229a3ef7bd7ffc705
gokul1630 <gokulgp3167@gmail.com>
2023-02-10 11:07:14 +0530

    Added route to decode the new url

--- main.go
+++ main.go
@@ -2,19 +2,46 @@ package main
 
 import (
 	"crypto/rand"
+	"encoding/json"
 	"math/big"
 	"net/http"
 )
 
 const letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+type Data struct {
+	Url string `json:"url"`
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./index.html")
 	})
 
+	http.HandleFunc("/url", handleNewUrl)
+
 	http.ListenAndServe(":3000", nil)
 
+}
+
+func handleNewUrl(w http.ResponseWriter, r *http.Request) {
+
+	var decodedUrl *Data
+	if r.Method == "POST" {
+		json.NewDecoder(r.Body).Decode(decodedUrl)
+	}
+
+	data := Data{
+		Url: generateUrl(10),
+	}
+
+	marshal, ok := json.Marshal(data)
+
+	if ok != nil {
+		panic(ok)
+	}
+
+	w.Write([]byte(marshal))
 }
 
 func generateUrl(n int) string {
